9e3aadd7fd39c6d22ad13b9914b66c502700eed9
yu-dai916 <respectyou0916@icloud.com>
2024-12-11 17:16:35 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:calendars).
+    permit(:date, :plan)
   end
 
   def getWeek
